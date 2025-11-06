20d047ed648815838d6f032e8ef9c7c3b9790d8e
MoonesMezher <moonesmezher050@gmail.com>
2025-11-06 18:50:18 +0300

    sw & l21

---
+++ sw.js
@@ -0,0 +1,59 @@
+const SITE_STATIC_CACHE_NAME = "pwa-cache-v1";
+const SITE_DYNAMIC_CACHE_NAME = "pwa-cache-dynamic-v1";
+
+const assets = [
+    "/",
+    "/index.html",
+    "/fallback.html",
+    "/logo.jpg",
+    "/tasks/index.html",
+    "/tables/index.html",
+    "/resources/index.html",
+    "manifest.json"
+]
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(SITE_STATIC_CACHE_NAME).then(cache => {
+            return cache.addAll(assets);
+        })
+    );
+
+    // console.log("service worker has been installed");
+});
+
+self.addEventListener("activate", event => {
+    // console.log("service worker has been activated");
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== SITE_STATIC_CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+})
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request).then(async res => {
+                    return caches.open(SITE_DYNAMIC_CACHE_NAME).then(cache => {
+                        cache.put(event.request.url, res.clone());
+
+                        return res;
+                    })
+                });
+            })
+            .catch(() => {
+                if(event.request.url.indexOf(".html") > -1) {
+                    return caches.match("/fallback.html")
+                }
+            })
+    );
+    /* console.log("service worker has been fetched", event); */
+});
